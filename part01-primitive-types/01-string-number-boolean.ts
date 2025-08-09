e771508979efb54e34c8deee4e2e816243a0fff5
nhannguyencsa <nhannguyencsa@gmail.com>
2025-08-09 18:27:58 -0500

    primitive types: string number and boolean

---
+++ part01-primitive-types/01-string-number-boolean.ts
@@ -0,0 +1,21 @@
+
+/* String types */
+var firstname: string = "John";
+// firstname = 1;//bao loi
+let automobile = "Toyota";
+const city = "New York";
+let students = 32;
+let studentAsString: string = students.toString();
+
+
+/* Number types */
+var age:number = 32;
+let year: number = 2025;
+const numberOfMembers = 39;
+// numberOfMembers = 79; //bao loi
+let sttringToNumber:number = parseInt("1975");
+
+/* Booleans */
+let isStudent: boolean = false;
+const alwaysStudent: boolean = true;
+let minimumAge = age >= 6 ? true : false;
