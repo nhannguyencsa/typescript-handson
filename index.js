6ab41345d2b29460382604d146ac8b3e52a984c1
nhannguyencsa <nhannguyencsa@gmail.com>
2025-08-10 09:03:23 -0500

    primitive types: bigint

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function addNumbers(a, b) {
-    return a + b;
-}
-console.log(addNumbers(2, 3));
-// console.log(addNumbers("text", 3));
+const safeInt = Number.MAX_SAFE_INTEGER;
+console.log(safeInt);
+const safeIntPLusOne = safeInt + 1;
+console.log(safeIntPLusOne);
+const safeIntPlusTwo = safeInt + 2;
+console.log(safeIntPlusTwo);
+let bigInt1 = BigInt(1234);
+let bigInt2 = 1234n;
+console.log(bigInt1);
+console.log(bigInt2);
+let c = bigInt1 - bigInt2;
+console.log(c);
+// let e:bigint = 123456.7n; //error
+// let f = Math.log(bigInt1)//error
