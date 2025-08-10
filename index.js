1d38b1f4eea2f084a458a536929c022e8e4d6e41
nhannguyencsa <nhannguyencsa@gmail.com>
2025-08-10 18:56:35 -0500

    type system: declaration annotation inference

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 "use strict";
 /*
-Type alias is a way to give a name to a type for reuse
+  Declaration: when to declare a type
+  annotation; when to assign a type
+  inference: try to infer the  type of variable
 */
 Object.defineProperty(exports, "__esModule", { value: true });
+//annotation; when to assign a type
 let firstName = "Nhan";
 let age = 99;
 let today = new Date();
 let unique = Symbol();
+function addNumbers(a, b) {
+    return a + b;
+}
+//inference: try to infer the  type of variable
+//Once sure the value has that type can you inference
+let finalResult = addNumbers(10, 15);
