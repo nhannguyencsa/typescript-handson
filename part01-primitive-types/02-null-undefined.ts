490264cf6487af4f3badfc4427987fb0c226fd59
nhannguyencsa <nhannguyencsa@gmail.com>
2025-08-09 19:26:56 -0500

    primitives type: null undefined

---
+++ part01-primitive-types/02-null-undefined.ts
@@ -0,0 +1,14 @@
+let user;
+console.log(user);
+ 
+let userRole: null;
+userRole = null;
+console.log(userRole); //Throw an error
+
+if(!userRole) {
+  console.log("This condition is true")
+}
+
+if(!user) {
+  console.log("This condition is true");
+}
