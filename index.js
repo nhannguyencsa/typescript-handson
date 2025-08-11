e6d4c39ac0550e038dec6d26b633fbc99f16b1d3
nhannguyencsa <nhannguyencsa@gmail.com>
2025-08-11 00:41:43 -0500

    type system: type hierarchy

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
 "use strict";
-/*
-  Conditional Type
-  - T extends U ? X : Y
-      - If type T can be assigned to type U → return X.
-      - Otherwise → return Y
-
-  - Practical Applications:
-    1. Choose a different type
-      type IsString<T> = T extends string ? "Yes" : "No";
-
-    2. Filter to keep only a type (Extract)
-      type OnlyString<T> = T extends string ? T : never;
-
-    3. Remove a type (Exclude)
-      type NoString<T> = T extends string ? never : T;
-
-    4. Transform a type
-      type ToReadonly<T> = { readonly [K in keyof T]: T[K] };
-
-    5. Infer a subtype (infer)
-      type ElementType<T> = T extends (infer U)[] ? U : T;
-*/
 Object.defineProperty(exports, "__esModule", { value: true });
+const throwError = (errorMessage) => {
+    throw new Error(errorMessage);
+};
+let string = ["a", "b"];
+let myFunc = () => 2;
