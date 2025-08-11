7024f44539f0bad068c5f7b12f288900d2a7477a
nhannguyencsa <nhannguyencsa@gmail.com>
2025-08-10 23:01:55 -0500

    type system: conditional type

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 "use strict";
+/*
+  Conditional Type
+  - T extends U ? X : Y
+      - If type T can be assigned to type U → return X.
+      - Otherwise → return Y
+
+  - Practical Applications:
+    1. Choose a different type
+      type IsString<T> = T extends string ? "Yes" : "No";
+
+    2. Filter to keep only a type (Extract)
+      type OnlyString<T> = T extends string ? T : never;
+
+    3. Remove a type (Exclude)
+      type NoString<T> = T extends string ? never : T;
+
+    4. Transform a type
+      type ToReadonly<T> = { readonly [K in keyof T]: T[K] };
+
+    5. Infer a subtype (infer)
+      type ElementType<T> = T extends (infer U)[] ? U : T;
+*/
 Object.defineProperty(exports, "__esModule", { value: true });
-let StringOrNumber = 1234;
-function print(input) {
-    if (input) {
-        console.log(input);
-    }
-    else {
-        console.log("Please input something to print");
-    }
-}
-print();
-print("Hello World!!");
