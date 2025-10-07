7cbf5b30c701b46bc13b09a9255eac70a852d309
nhannguyencsa <nhannguyencsa@gmail.com>
2025-10-07 12:40:19 -0500

    array and enum: strictly typing array

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-var hybridAnimal = {
-    name: "Max",
-    color: "white",
-    purrs: false,
-    barks: true
-};
-console.log(hybridAnimal);
+var a = [1, 2, 3]; //number array
+var b = ["a", "b", "c"]; //array with generic syntax
+var c = ["a", 1]; //array with union type
+var airplane = [{
+        model: "Airbus A380",
+        flightNumber: "A2201",
+        timeOfDeparture: new Date(),
+        timeOfArrival: new Date(),
+        caterer: {
+            name: "Special Food LLC",
+            address: "484, Some Street, Alabama",
+            phone: 123456789,
+        },
+        seats: {
+            A1: "John Doe",
+            A2: "Mark Doe"
+        }
+    }];
+console.log(airplane);
